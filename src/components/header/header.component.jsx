2b6889e373fff4ead7f643e8b64cf7437fc9cc82
Gautam <gautam@grappus.com>
2020-01-25 23:54:41 +0530

    Integrated firebase, created signin page with signin component and signin with google funcitonality

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { auth } from "../../firebase/firebse.utils";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 import "./header.styles.scss";
 
-const Header = () => (
+const Header = ({ currentUser }) => (
   <div className="header">
     <Link className="logo-container" to="/">
-      <Logo className="logo" />
+      <Logo className="logo" title="logo" />
     </Link>
     <div className="options">
       <Link className="option" to="/shop">
@@ -16,6 +17,17 @@ const Header = () => (
       </Link>
       <Link className="option" to="/shop">
         CONTACT
+      </Link>
+      <Link className="option" to="/signin">
+        {currentUser ? (
+          <div className="option" onClick={() => auth.signOut()}>
+            SIGN OUT
+          </div>
+        ) : (
+          <Link className="option" to="/signin">
+            SIGN IN
+          </Link>
+        )}
       </Link>
     </div>
   </div>
